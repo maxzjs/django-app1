8c98cac88ed2daaf92b744ede7dde2d5b9b3d558
Virtualzjs <13843321+maxzjs@users.noreply.github.com>
2025-07-20 23:22:58 +0800

    Initial commit

---
+++ app1/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+
+
+
+def custom_404(request, exception):
+    # 这里的'home'是你想要重定向到的路由名称，你可以根据需要修改
+    return redirect('login')
+
+def login(request):
+    return render(request, 'app1/login.html')
+
+
+def index(request):
+    return render(request, 'app1/index.html')
+
+
+
+
+
